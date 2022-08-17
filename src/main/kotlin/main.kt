8f5ac1428ca4ed02675deee1e2b22d09b6496914
TRINI FENG <trini.feng23@CLS-APP-04.local>
2022-08-17 08:03:06 -0500

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,22 @@
+fun main(){
+    var myInt: Int = 5
+    var myLong = 2480943L
+    var myShort: Short = 48
+    var myByte: Byte = 4
+    var myDouble: Double = 43.23213235325
+    var myFloat: Float = 3.14F
+    var myChar: Char = 'h'
+    var myBoolean: Boolean = true
+
+    var myString = "hello"
+
+    println("This is an int: $myInt")
+    println("This is a long: $myLong")
+    println("This is a short: $myShort")
+    println("This is a byte: $myByte")
+    println("This is a double: $myDouble")
+    println("This is a float: $myFloat")
+    println("This is a char: $myChar")
+    println("This is a boolean: $myBoolean")
+    println("This is a string: $myString")
+}
